032066ab22b7d6b278e49be82ed78cb9198ddb7d
miauliong <changmiauliong1409@gmail.com>
2023-10-14 23:01:52 +0700

    ubah warna tulisa

--- main.js
+++ main.js
@@ -244,7 +244,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#791103",
         ease: Expo.easeOut,
       },
       0.1,
